c85c1912497f8c038b3b204f7356f4de40559f8d
kenji matsuoka <egs@firespeed.org>
2020-07-27 05:59:15 +0900

    Use Jetpack activityForResult and fix for Androd11.

--- app/src/main/java/org/firespeed/androidcamera/MainActivity.kt
+++ app/src/main/java/org/firespeed/androidcamera/MainActivity.kt
@@ -4,12 +4,13 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
@@ -20,12 +21,20 @@ import java.util.concurrent.Executors
 
 
 class MainActivity : AppCompatActivity() {
-    private var preview: Preview? = null
     private var imageCapture: ImageCapture? = null
     private var camera: Camera? = null
 
     private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
+
+    private val requestPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            Toast.makeText(this, "Require Permissions.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +44,7 @@ class MainActivity : AppCompatActivity() {
         if (allPermissionsGranted()) {
             startCamera()
         } else {
-            ActivityCompat.requestPermissions(
-                    this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+            requestPermission.launch(REQUIRED_PERMISSIONS)
         }
 
         // Setup the listener for take photo button
@@ -55,7 +63,7 @@ class MainActivity : AppCompatActivity() {
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
             // Preview
-            preview = Preview.Builder().build()
+            val preview = Preview.Builder().build()
             imageCapture = ImageCapture.Builder().build()
             // Select back camera
             val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
@@ -66,7 +74,7 @@ class MainActivity : AppCompatActivity() {
 
                 // Bind use cases to camera
                 camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
-                preview?.setSurfaceProvider(viewFinder.createSurfaceProvider())
+                preview.setSurfaceProvider(viewFinder.createSurfaceProvider())
             } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
@@ -104,27 +112,10 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    fun getOutputDirectory(): File {
-        val mediaDir = externalMediaDirs.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
-        }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else filesDir
-    }
-
-    override fun onRequestPermissionsResult(
-            requestCode: Int, permissions: Array<String>, grantResults:
-            IntArray) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(this,
-                        "Permissions not granted by the user.",
-                        Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
+    private fun getOutputDirectory(): File {
+        val file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "sample")
+        file.mkdirs()
+        return file
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
@@ -135,7 +126,6 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "CameraXBasic"
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
